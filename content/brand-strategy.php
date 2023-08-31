3334dfe4048adb7342575f70ed6022d07a3b82a9
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-01 01:08:43 +0200

    Update brand-strategy.php

--- content/brand-strategy.php
+++ content/brand-strategy.php
@@ -1,3 +1,25 @@
 <section id="brand-strategy">
 <h2 class="heading-1">3. Brand Strategy</h2>
+<h3 class="heading-2">3.1. Voice</h3>
+<p>The content expressed by our brand must be informative and engaging, proactive in the design community.</p>
+<h3 class="heading-2">3.2. Tone</h3>
+<h3 class="heading-2">3.3. Keywords</h3>
+<ul class="hanging-list arrow-bullet">
+    <li>Radical</li>
+    <li>Creative</li>
+    <li>Playful</li>
+    <li>Innovative</li>
+    <li>User-friendly</li>
+    <li>Clean</li>
+    <li>Distinctive</li>
+    <li>Business-oriented</li>
+</ul>
+<h3 class="heading-2">3.4. Creative Concept</h3>
+<h4 class="heading-3">Where business & creativity come together</h4>
+<p>Similar to the Keywords, but more expressive and distinctive, the Creative Concept is a word or phrase that represents the desired emotional response from the target public when interacting with the Brand.</p>
+<p>Unlike a Slogan, a Creative Concept is not necessarily developed for public use, although it can be used publicly. Its primary purpose is to be used internally as a mantra to guide the creative process when sketching a Logo, copywriting a slogan, designing an ad, or when developing any other type of Brand Communication. This ensures that all elements of the Brand will convey a consistent message.</p>
+<h3 class="heading-2">3.5. Moodboard</h3>
+<p>The Moodboard contains a collection of images the can be associated with the industry and the lifestyle of the target audience. It helps identify different colours and shapes with the aim of narrowing down a certain direction for the look-and-feel of the brand.</p>
+<h3 class="heading-2">3.6. Naming</h3>
+<h3 class="heading-2">3.7. Brand Architecture</h3>
 </section>
