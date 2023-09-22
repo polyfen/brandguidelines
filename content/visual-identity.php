28dea1005ca618411754b16adbef7d83d685a470
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-23 00:08:44 +0200

    Added support colors

--- content/visual-identity.php
+++ content/visual-identity.php
@@ -1,4 +1,24 @@
 <section id="visual-identity">
-<h2 class="heading-1">4. Visual Identity</h2>
-<h3>4.1. Logo System</h3>
+<h2 class="heading-1">4. Visual identity</h2>
+<h3 class="heading-2">4.1. Logo system</h3>
+<h4 class="heading-3">4.1.1. Ideogram</h4>
+<p>Polyfen's Ideogram represents the conjunction of two worlds, business and creativity.</p>
+<p>The Ideogram is the visual representation of Polyfen's brand in its most basic and essential form. It's reserved for use exclusively on reduced sizes and square or circle-shaped canvases.</p>
+<h4 class="heading-2">4.1.2. Emblem</h4>
+<p>The Emblem is the preferred visual representation of the Polyfen brand. It must be the first choice when choosing a logo variation, provided that the canvas is rectangular and the logo can be placed at an adequate size.</p>
+<h3 class="heading-2">4.2. Color scheme</h3>
+<h4 class="heading-3">4.2.1. Accent color</h3>
+<div class="color-swatch-content">
+<div class="color-swatch" style="background-color: #7E2EF5;color: white;">Polyfen<br>Purple</div>
+<div class="color-details">
+    <ul class="hanging-list">
+        <li>HEX: #7E2EF5</li>
+        <li>RGB: 126, 46, 245</li>
+        <li>CMYK: 49, 81, 0, 0</li>
+    </ul>
+</div>
+</div>
+<p><strong>Polyfen Purple</strong> is the primary accent hue and must be used sparingly. It's reserved exclusively for buttons, links, and other important information.</p>
+<h4 class="heading-3">4.2.2. Support colors</h3>
+<h5 class="heading-4">Slate</h5>
 </section>
